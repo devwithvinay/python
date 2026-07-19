0fb192143b23308f26836f391281920242852580
vinay-here <krvinay785@gmail.com>
2026-07-19 09:41:39 +0530

    typeCastinf , Arithmetic

---
+++ 02/2.typecasting.py
@@ -0,0 +1,18 @@
+# TypeCasting :- The process of converting a variable from one data type to another - str() , int() , float() , bool()
+
+name = "Vinay Kumar"
+age = 23 
+cgpa = 7.5
+is_graduated = True
+
+type(name)
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_graduated))
+
+age = float(age)
+print(age) # 23.0
+
+cgpa = int(cgpa)
+print(cgpa) # 7
---
+++ 02/3.arithmetic.py
@@ -0,0 +1,58 @@
+# Arithmetic  = + Addition
+#               - Subraction
+#               * Mutiplication
+#               / Division (return a float)
+#               // Integer Division 
+#               % Remainder
+
+#Assignment Operator :- += , -=
+
+
+#Addition
+friends = 5
+
+friends += 7
+print(friends)
+
+# friends = friends + 1 
+# we are NOT using --> "friends++" <-- it gives error
+
+
+#Subtraction
+pizza = 5
+
+pizza -= 2
+print(f"pizza is {pizza}")
+
+#Mutiplication
+balls = 3 
+
+balls*=2
+print(f"Nos of Balls is: {balls}")
+
+# Division (return float)
+
+Apples = 5
+
+Apples /= 2
+Apples = Apples / 2
+print(f"Apples Divided by 2 is: {Apples}") # 2.5
+
+# Integer Division 
+
+Bottles = 7
+
+Bottles //= 2
+print(f"No.s of Bottles is: {Bottles}") # 3
+
+# Remainders 
+
+Watches = 5
+
+Watches %= 3
+print(f"remainder is :{Watches}") # 2
+
+
+
+
+
